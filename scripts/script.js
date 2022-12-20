edd98d052a1cb2ffee1afdc976b406d1a161d50d
Richard Goshen <rick.goshen@gmail.com>
2022-12-20 15:04:38 -0700

    add proper declaration to year variable

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,27 @@
-const currentYear = document.getElementById("current-year");
+const currentYear = document.getElementById('current-year');
 
 // update copyright date to current year
-year = new Date().getFullYear();
+const year = new Date().getFullYear();
 currentYear.innerHTML = year;
 
 // fade animation - add fade class to elements
-$(window).on("load", function () {
+$(window).on('load', function () {
   $(window)
     .scroll(function () {
       const windowBottom = $(this).scrollTop() + $(this).innerHeight();
-      $(".fade").each(function () {
+      $('.fade').each(function () {
         /* Check the location of each desired element */
         const objectBottom = $(this).offset().top + $(this).outerHeight();
 
         /* If the element is completely within bounds of the window, fade it in */
         if (objectBottom < windowBottom) {
           //object comes into view (scrolling down)
-          if ($(this).css("opacity") == 0) {
+          if ($(this).css('opacity') == 0) {
             $(this).fadeTo(500, 1);
           }
         } else {
           //object goes out of view (scrolling up)
-          if ($(this).css("opacity") == 1) {
+          if ($(this).css('opacity') == 1) {
             $(this).fadeTo(500, 0);
           }
         }
@@ -42,18 +42,18 @@ function recheck() {
     clearTimeout(timer);
   }
 
-  $(".fly").each(function () {
+  $('.fly').each(function () {
     var block = $(this);
     var block_top = block.offset().top;
     var block_height = block.height();
 
     if (block_top < view_port_e) {
       timer = setTimeout(function () {
-        block.addClass("show-block");
+        block.addClass('show-block');
       }, 100);
     } else {
       timer = setTimeout(function () {
-        block.removeClass("show-block");
+        block.removeClass('show-block');
       }, 100);
     }
   });
@@ -74,13 +74,13 @@ $(function () {
 // smoove effects - add block to class and data-EFFECT attributes
 
 // initialize
-$(".block").smoove();
+$('.block').smoove();
 
-$(".block").smoove({
+$('.block').smoove({
   offset: 150,
   opacity: 0,
-  transition: "all 1s ease, opacity 1.5s ease",
-  transformStyle: "preserve-3d",
+  transition: 'all 1s ease, opacity 1.5s ease',
+  transformStyle: 'preserve-3d',
   transformOrigin: false,
   perspective: 1000,
   min_width: 768,
