ff153201f8971d47fd36da535e8aac9d58558ad4
rgoshen <rick.goshen@gmail.com>
2024-10-25 14:49:57 -0700

    finish setup proj for react, typescript
    Signed-off-by: rgoshen <rick.goshen@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Vite + React</h1>
+      </div>
+      <div>
+        <h2>Personal Portfolio</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
